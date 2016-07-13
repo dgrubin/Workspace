6be6a3fc49bfc3e9ef271229a847ded3f28ae167
dgrubin <dominik.grubinger@gmail.com>
2016-07-13 19:29:17 +0200

    Verzeichnisstruktur angelegt

---
+++ C/2016-07-13_Hello-World/README.md
@@ -0,0 +1,5 @@
+#owner:		DGRUBIN
+#date:		2016-07-13
+#project:	Hello-World
+
+#files:		helloworld.c
---
+++ C/2016-07-13_Hello-World/helloworld.c
@@ -0,0 +1,15 @@
+/***********************************************************************
+*	Author:		DGRUBIN
+*	Date:		2016-07-13
+*	File:		helloworld.c
+*
+*	Purpose:	my first program, to start easy: helloworld.c
+*
+***********************************************************************/
+
+#include <stdio.h>
+
+int main (){
+	printf("Hello, world!\n");
+	return 0;
+}
